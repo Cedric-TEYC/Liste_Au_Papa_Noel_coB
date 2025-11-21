7dabef31ca2322aee2e990a70ee01aa1e671c11b
Cedric-TEYC <contact.axis.architecture@gmail.com>
2025-11-21 11:24:10 +0100

    Refactor Firebase initialization and database access

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
-// Configuration Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD8Lgk1P2FzIjyVMw268C_YXvSJ76aiRqU",
-    authDomain: "listecadeauxcob.firebaseapp.com",
-    databaseURL: "https://listecadeauxcob-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "listecadeauxcob",
-    storageBucket: "listecadeauxcob.firebasestorage.app",
-    messagingSenderId: "494154955318",
-    appId: "1:494154955318:web:deed82556327838ca4b697"
+  apiKey: "AIzaSyD8Lgk1P2FzIjyVMw268C_YXvSJ76aiRqU",
+  authDomain: "listecadeauxcob.firebaseapp.com",
+  databaseURL: "https://listecadeauxcob-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "listecadeauxcob",
+  storageBucket: "listecadeauxcob.firebasestorage.app",
+  messagingSenderId: "494154955318",
+  appId: "1:494154955318:web:bd84a246370a6b87a4b697"
 };
 
-// Initialisation Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
